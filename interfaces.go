3fd2bc1094be0ac3f1f74404fa76857b9b854763
wangbaojin <904026435@qq.com>
2018-10-08 23:35:58 +0800

    test CI

--- interfaces.go
+++ interfaces.go
@@ -2,6 +2,7 @@
 // This file is part of the go-ethereum library.
 //
 //
+//
 // The go-ethereum library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
